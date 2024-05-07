e87e91199c450e876f755c19fecef7a29d891756
musiienko25 <devmusiienko@gmail.com>
2024-05-07 20:19:27 +0300

    courses page

---
+++ app/(routes)/courses/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Courses = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <div>
+        <div> Courses</div>
+
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Courses;

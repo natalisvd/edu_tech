b8e2dd368bc96e2d699964d4371b626b8bd1a08d
musiienko25 <devmusiienko@gmail.com>
2024-05-07 20:19:27 +0300

    courses page

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,32 @@ export default async function Home() {
   }
 
   return (
-    <div className="container mx-auto py-8">
+    <div
+      className="container mx-auto py-8"
+      style={{
+        backgroundImage:
+          "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+      }}
+    >
       <div>
         <LogoutButton />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div>
+        <div className="hero min-h-screen">
+          <div className="hero-content text-center">
+            <div className="max-w">
+              <h1 className="text-3xl font-bold">
+                Your Path to Senior Developer and Beyond
+              </h1>
+              <p className="py-6"></p>
+              <Link href="/courses" className="btn btn-active btn-primary">
+                Browse Our Courses
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <Link href="/frontend">
           <div className="bg-base-100 border border-base-200 p-8 rounded shadow-md cursor-pointer transition-colors duration-300 hover:bg-base-200">
             <h2 className="text-xl font-bold mb-4">Frontend</h2>
@@ -36,7 +57,7 @@ export default async function Home() {
             <p>Click and take the full-stack test</p>
           </div>
         </Link>
-      </div>
+      </div> */}
       {/* <Button /> */}
     </div>
   );

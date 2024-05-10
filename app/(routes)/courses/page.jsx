6c0d40eecc732cdb2141d6648ca0c4f50636813a
musiienko25 <devmusiienko@gmail.com>
2024-05-10 21:19:24 +0300

    revwrite courses

--- app/(routes)/courses/page.jsx
+++ app/(routes)/courses/page.jsx
@@ -4,12 +4,14 @@ import Card from "./Card";
 import Select from "../../components/Select/Select";
 import { createClient } from "@/utils/supabase/server";
 import { getCourses } from "./action";
+import Input from "./Input";
 
 const Courses = async () => {
   const { courses } = await getCourses();
-  console.log('courses getServerSideProps', courses)
+
   // const supabase = createClient();
   // let { data: courses, error } = await supabase.from("courses").select("*");
+
   return (
     <div className="container mx-auto py-8">
       <div className="flex justify-center">
@@ -17,11 +19,7 @@ const Courses = async () => {
           <div>{/* <Data /> */}</div>
           <div className="mb-10"> Frontend & Fullstack Engineering Courses</div>
 
-          <input
-            type="text"
-            placeholder="Search for Courses"
-            className="input input-bordered w-full max-w"
-          />
+          <Input corses={courses} />
           <Select />
           <div className="flex flex-wrap justify-center">
             {courses.map((course) => (

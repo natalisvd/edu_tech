f96eef579ffa0aaa39414dc3e68e393cadca3417
musiienko25 <devmusiienko@gmail.com>
2024-05-14 19:35:22 +0300

    lesson page

--- app/(routes)/courses/[id]/page.tsx
+++ app/(routes)/courses/[id]/page.tsx
@@ -1,13 +1,48 @@
-"use client";
+import { useParams, usePathname } from "next/navigation";
+import { getCoursesById } from "../action";
+import { headers } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
 
-// import { useRouter } from "next/router";
+const Page = async () => {
+  const supabase = createClient();
+  const heads = headers();
+  const pathname = heads.get("x-pathname");
+  console.log(pathname?.match(/\/courses\/(\d+)/));
+  // const match = pathname?.match(/\/courses\/(\d+)/);
+  // const courseId = match ? match[1] : null;
+  // console.log(courseId);
+  // const params = useParams<{
+  //   [x: string]: any;
+  //   tag: string;
+  //   item: string;
+  // }>();
+  // console.log(pathname);
+  // console.log(params.id);
+  // getCoursesById(params.id);
+  let { data: course, error } = await supabase
+    .from("courses")
+    .select("*")
+    .eq("id", 1);
+  // @ts-ignore
 
-import { usePathname } from "next/navigation";
-
-const Page = () => {
-  const pathname = usePathname();
-  console.log(pathname);
-  return <div>Page </div>;
+  console.log(course[0].Description);
+  if (course && course.length > 0) {
+    console.log(course[0].Description);
+    return (
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold"> Title</h1>
+            <p className="py-6">{course[0].Description}</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    console.log("Course not found");
+    return <div>Course not found</div>;
+  }
 };
 
 export default Page;
--- app/(routes)/courses/action.tsx
+++ app/(routes)/courses/action.tsx
@@ -1,12 +1,10 @@
-import Data from "./data";
-import Card from "./Card";
-import Select from "../../components/Select/Select";
 import { createClient } from "@/utils/supabase/server";
+import { createClient as creaClientonClien } from "@/utils/supabase/client";
 
 export async function getCourses() {
   const supabase = createClient();
   const { data: courses, error } = await supabase.from("courses").select("*");
-  console.log('courses', courses)
+  console.log("courses", courses);
 
   if (error) {
     console.error("Error fetching courses:", error.message);
@@ -18,4 +16,14 @@ export async function getCourses() {
   return {
     courses: courses || [],
   };
+}
+
+export async function getCoursesById(id: number) {
+  const supabase = creaClientonClien();
+  let { data: course, error } = await supabase
+    .from("courses")
+    .select("*")
+    .eq("id", id);
+  // @ts-ignore
+  console.log(course[0]);
 }

2a8771ccfdb0b1498d618cf9250c1910d1561654
musiienko25 <devmusiienko@gmail.com>
2024-05-17 17:52:59 +0300

    added edit func

---
+++ app/(routes)/courses/components/Button/button.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { FC } from "react";
+
+import { createClient } from "@/utils/supabase/client";
+
+const updateColumn = async (description: any, oldDescription: any) => {
+  console.log(description);
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("courses")
+    .update({ Description: description })
+    .eq("Description", oldDescription)
+    .select();
+};
+
+interface ButtonProps {
+  description: string;
+  oldDescription: string;
+}
+
+const Button: FC<ButtonProps> = ({ description, oldDescription }) => {
+  console.log("descriptionprops", description);
+  return (
+    <button
+      className="btn btn-success"
+      onClick={() => updateColumn(description, oldDescription)}
+    >
+      Save
+    </button>
+  );
+};
+
+export default Button;

bedd1fc019ac03ff1275ea3c3ba4217a7ba52b45
Anna Pravdiukova <annagekata82@gmail.com>
2024-05-22 18:43:08 +0300

    added config example for correct usage with Next Image component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // -- To use a remote image with Next <Image /> need to define remote storage in config --
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.daisyui.com', // -- change the hostname to db storage source
+        port: '',
+        pathname: '/images/**', // -- edit storage path if it needed
+      },
+    ],
+  },
+};
 
 export default nextConfig;

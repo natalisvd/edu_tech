28c8b76cb5c18576c8795f5f17e6be4eaaa6d7f8
musiienko25 <devmusiienko@gmail.com>
2024-06-07 17:44:15 +0300

    fixed errors

--- app/(user_layout)/my_skills/components/SelectNewSkill.tsx
+++ app/(user_layout)/my_skills/components/SelectNewSkill.tsx
@@ -1,21 +1,28 @@
-import { forwardRef } from 'react'
-import { Skills } from '../types'
+import { forwardRef } from "react";
+import { Skills } from "../types";
 
 interface SelectSkillProps {
-  skillsList: Skills
-  defaultValue: number
+  skillsList: Skills;
+  defaultValue: number;
 }
-export type Ref = HTMLSelectElement
 
-export const SelectSkill = forwardRef<Ref, SelectSkillProps>(({ defaultValue, skillsList, ...rest }, ref) => {
-  return (
-    <select className='select select-bordered w-full' ref={ref} {...rest}>
-      <option disabled value={defaultValue}>
-        Choose skill
-      </option>
-      {skillsList.map((skill) => (
-        <option key={skill.id} value={skill.id}>{skill.skill_name}</option>
-      ))}
-    </select>
-  )
-})
+export type Ref = HTMLSelectElement;
+
+export const SelectSkill = forwardRef<Ref, SelectSkillProps>(
+  ({ defaultValue, skillsList, ...rest }, ref) => {
+    return (
+      <select className="select select-bordered w-full" ref={ref} {...rest}>
+        <option disabled value={defaultValue}>
+          Choose skill
+        </option>
+        {skillsList.map((skill) => (
+          <option key={skill.id} value={skill.id}>
+            {skill.skill_name}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
+
+SelectSkill.displayName = "SelectSkill";

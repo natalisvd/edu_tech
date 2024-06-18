a2df398a65d0356ea1f69835dc3e940de9215f95
musiienko25 <devmusiienko@gmail.com>
2024-06-18 12:12:43 +0300

    minor changes

--- app/(user_layout)/(admins)/manage_users/page.tsx
+++ app/(user_layout)/(admins)/manage_users/page.tsx
@@ -1,9 +1,13 @@
+import { createClient } from "@/utils/supabase/server";
+
 const Page = async () => {
-  return (
-    <div className="text-xl">
-      admin page
-    </div>
-  )
-}
+  const supabase = createClient();
+  const {
+    data: { users },
+    error,
+  } = await supabase.auth.admin.listUsers();
+  console.log("users", users);
+  return <div className="text-xl">Page</div>;
+};
 
 export default Page;

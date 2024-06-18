cb50983f885270cf402c6e286d5f52d56603cb97
musiienko25 <devmusiienko@gmail.com>
2024-06-18 12:26:14 +0300

    fixed auth error

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,57 +1,74 @@
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
-import { UserMenu } from './UserMenu'
-import { TeamleadMenu } from './TeamleadMenu'
-import { AdminMenu } from './AdminMenu'
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { UserMenu } from "./UserMenu";
+import { TeamleadMenu } from "./TeamleadMenu";
+import { AdminMenu } from "./AdminMenu";
 
-// -- https://github.com/supabase/postgrest-js/issues/303 
+// -- https://github.com/supabase/postgrest-js/issues/303
 // -- While the issue about supabase types for '.single()' and '.maybeSingle()' is not completed
 // –- I will use this type definition for query result
 
-type RoleResponse = { role_name?: string }
+type RoleResponse = { role_name?: string };
 
 async function getRole() {
-  const supabase = createClient()
-  const { data: { user } } = await supabase.auth.getUser()
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  if (!user) redirect('/login')
+  if (!user) redirect("/login");
 
-  const { data: role_to_user, error, status } = await supabase
-    .from('role_to_user')
-    .select(`
+  const {
+    data: role_to_user,
+    error,
+    status,
+  } = await supabase
+    .from("role_to_user")
+    .select(
+      `
       role (
         role_name
       )
-    `)
-    .eq('user_id', user.id)
+    `
+    )
+    .eq("user_id", user.id)
     .limit(1)
-    .maybeSingle()
+    .maybeSingle();
+
   if (error && status !== 406) {
-    console.log(error.message)
-    return 'Developer'
+    console.log("Error:", error.message);
+    return "Developer";
   }
-  const { role_name } = role_to_user?.role as RoleResponse
+
+  console.log("role_to_user:", role_to_user);
+
+  const role_name = role_to_user?.role?.role_name;
   if (role_name) {
-    return role_name
+    return role_name;
   }
-  return 'Developer'
+
+  return "Developer";
 }
 
 export const Sidebar = async () => {
-  const userRole = await getRole()
-  const isTeamleadOrAdmin = ['Teamlead', 'Admin'].includes(userRole)
+  const userRole = await getRole();
+  const isTeamleadOrAdmin = ["Teamlead", "Admin"].includes(userRole);
 
   return (
-    <div className='drawer-side mt-16'>
-      <label htmlFor='left-sidebar-menu' aria-label='close sidebar' className='drawer-overlay' />
-        <ul className='menu p-4 w-80 min-h-full bg-base-100 text-base-content'>
-          {/* All users links */}
-          <UserMenu />
-          {/* Teamlead links */}
-          {isTeamleadOrAdmin && <TeamleadMenu />}
-          {/* Admin links */}
-          {userRole === "Admin" && <AdminMenu />}
-        </ul>
+    <div className="drawer-side mt-16">
+      <label
+        htmlFor="left-sidebar-menu"
+        aria-label="close sidebar"
+        className="drawer-overlay"
+      />
+      <ul className="menu p-4 w-80 min-h-full bg-base-100 text-base-content">
+        {/* All users links */}
+        <UserMenu />
+        {/* Teamlead links */}
+        {isTeamleadOrAdmin && <TeamleadMenu />}
+        {/* Admin links */}
+        {userRole === "Admin" && <AdminMenu />}
+      </ul>
     </div>
-  )
-}
+  );
+};

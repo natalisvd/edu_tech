cd8367841ee8e7a83b04a4fd1f54bc7be43dbea7
musiienko25 <devmusiienko@gmail.com>
2024-06-18 17:08:54 +0300

    refresh admin page

--- app/(user_layout)/(admins)/manage_users/page.tsx
+++ app/(user_layout)/(admins)/manage_users/page.tsx
@@ -5,17 +5,31 @@ const Page = async () => {
   const supabase = createClient();
 
   let { data: profiles, error } = await supabase.from("profiles").select("*");
-
+  console.log(profiles);
   return (
-    <div className="text-xl">
-      {profiles?.map((user) => (
-        <div className="flex mt-5">
-          <div>{user.first_name}</div>
-          <div>
-            <Button id={user.id} />
-          </div>
-        </div>
-      ))}
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>name</th>
+              <th>admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {profiles?.map((user) => (
+              <tr key={user.id}>
+                <th></th>
+                <td>{user.first_name ? user.first_name : "some user"}</td>
+                <td>
+                  {user.role_id === "" ? "admin" : <Button id={user.id} />}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

fe2bf91cb49de67f9a4401a8addbe711924aff27
musiienko25 <devmusiienko@gmail.com>
2024-06-19 13:49:21 +0300

    refreshed desighn admin page

--- app/(user_layout)/(admins)/manage_users/page.tsx
+++ app/(user_layout)/(admins)/manage_users/page.tsx
@@ -13,30 +13,32 @@ const Page = async () => {
   }
 
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <div className="overflow-x-auto">
-        <table className="table">
+        <table className="min-w-full bg-white border border-gray-300">
           <thead>
-            <tr>
-              <th></th>
-              <th>name</th>
-              <th>admin</th>
-              <th>team</th>
+            <tr className="w-full bg-gray-200 text-left">
+              <th className="px-4 py-2 border">#</th>
+              <th className="px-4 py-2 border">Name</th>
+              <th className="px-4 py-2 border">Admin</th>
+              <th className="px-4 py-2 border">Team</th>
             </tr>
           </thead>
           <tbody>
-            {profiles?.map((user) => (
-              <tr key={user.id}>
-                <th></th>
-                <td>{user.first_name ? user.first_name : "some user"}</td>
-                <td>
+            {profiles?.map((user, index) => (
+              <tr key={user.id} className="hover:bg-gray-100">
+                <td className="px-4 py-2 border">{index + 1}</td>
+                <td className="px-4 py-2 border">
+                  {user.first_name ? user.first_name : "some user"}
+                </td>
+                <td className="px-4 py-2 border">
                   {user.role_id === 1 ? (
-                    <span>admin</span>
+                    <span className="text-green-600">admin</span>
                   ) : (
                     <Button id={user.id} />
                   )}
                 </td>
-                <td>{getTeam(user.id)}</td>
+                <td className="px-4 py-2 border">{getTeam(user.id)}</td>
               </tr>
             ))}
           </tbody>

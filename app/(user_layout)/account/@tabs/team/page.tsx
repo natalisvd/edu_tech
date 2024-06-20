e77733be52cc78dea2fa34ca71742b3bbe27cb86
musiienko25 <devmusiienko@gmail.com>
2024-06-20 19:13:52 +0300

    added team group to the settings

--- app/(user_layout)/account/@tabs/team/page.tsx
+++ app/(user_layout)/account/@tabs/team/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div> Create new Team</div>;
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Page() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  let { data: teams, error } = await supabase
+    .from("teams")
+    .select("*")
+    .eq("user_id", user.id);
+
+  console.log(teams);
+
+  return (
+    <div className="flex">
+      {" "}
+      Your team
+      {teams?.map((team) => {
+        return <div>{team.team_name}</div>;
+      })}
+    </div>
+  );
 }

d53f9a46d22b56eb54d06473b39c2b5753b9e8d0
musiienko25 <devmusiienko@gmail.com>
2024-06-24 18:43:45 +0300

    added member search

--- app/(user_layout)/account/@tabs/team/page.tsx
+++ app/(user_layout)/account/@tabs/team/page.tsx
@@ -1,4 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
+import { getUsers } from "./action";
+import Users from "./users";
 
 export default async function Page() {
   const supabase = createClient();
@@ -11,15 +13,22 @@ export default async function Page() {
     .select("*")
     .eq("user_id", user.id);
 
-  console.log(teams);
-
+  const users = await getUsers();
+  console.log("users", users);
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       {" "}
-      Your team
-      {teams?.map((team) => {
-        return <div>{team.team_name}</div>;
-      })}
+      <div className="mb-5">
+        {" "}
+        Your team
+        {teams?.map((team) => {
+          return <div>{team.team_name}</div>;
+        })}
+      </div>
+      <div>
+        {" "}
+        <Users users={users} />
+      </div>
     </div>
   );
 }

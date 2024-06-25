b51ab01d36096aeff1d60b21a10b4d954a31410f
musiienko25 <devmusiienko@gmail.com>
2024-06-25 19:53:54 +0300

    new markup for teamnames

--- app/(user_layout)/account/@tabs/team/page.tsx
+++ app/(user_layout)/account/@tabs/team/page.tsx
@@ -19,12 +19,19 @@ export default async function Page() {
   const teamNames = teams?.map((team) => team.team_name) || [];
 
   return (
-    <div className="flex flex-col">
-      <div className="mb-5">
-        Your team
-        {teamNames.map((teamName, index) => (
-          <div key={index}>{teamName}</div>
-        ))}
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">Your Team</h2>
+        <div className="space-y-2">
+          {teamNames.map((teamName, index) => (
+            <div
+              key={index}
+              className="p-2 bg-white shadow rounded text-gray-800"
+            >
+              {teamName}
+            </div>
+          ))}
+        </div>
       </div>
       <div>
         <Users users={users} teamNames={teamNames} />

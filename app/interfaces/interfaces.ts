ebd06b5935188ff369e68052bf8ad66f937056f8
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-07-29 10:45:25 +0300

    Update interfaces

--- app/interfaces/interfaces.ts
+++ app/interfaces/interfaces.ts
@@ -1,17 +1,33 @@
 //TODO
 export interface ITeam {
-    team_id: string;
+  id: string;
+  team_id: string;
+  user_id: string;
 }
 
 export interface IUser {
   id: string;
+  username: string;
   first_name: string;
   last_name: string;
-  avatar_url?: string
+  avatar_url?: string;
   role_id?: number;
   team_id?: string;
 }
 
+export interface ITeamLeader {
+  id: string;
+  leader_id: string;
+  team_id?: string;
+}
+
+export interface ITeamList {
+  team_name: string;
+}
+
 export interface IUserWithTeam extends IUser {
+  teams: ITeam[];
+}
+export interface ITeamLeaderWithTeams extends ITeamLeader {
   teams: ITeam[];
 }

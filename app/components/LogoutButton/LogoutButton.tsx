fab6e87d10f65c481eaab2a3fb1bd90b62dcc3c0
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-08-14 11:19:19 +0300

    Logout

--- app/components/LogoutButton/LogoutButton.tsx
+++ app/components/LogoutButton/LogoutButton.tsx
@@ -1,11 +1,19 @@
-'use client'
+"use client";
+
+import { useAppDispatch } from "@/app/store/hooks";
+import { logout } from "@/app/store/slices/userSlice";
 
 export const LogoutButton = () => {
+  const dispatch = useAppDispatch();
+
   return (
-    <form action="/auth/logout" method="post">
-      <button className="btn btn-primary" type="submit">
-        Log out
-      </button>
-    </form>
-  )
-}
+    <button
+      className="btn btn-primary"
+      onClick={() => {
+        dispatch(logout());
+      }}
+    >
+      Log out
+    </button>
+  );
+};

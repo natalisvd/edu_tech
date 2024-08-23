662cd3f73bbd77710f6763deb4fac845782ca28e
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-08-23 10:46:32 +0300

    Fix access in card

--- app/(user_layout)/account/@tabs/team/components/TeamList.tsx
+++ app/(user_layout)/account/@tabs/team/components/TeamList.tsx
@@ -1,5 +1,8 @@
 import { getAvatarUrl } from "@/app/helpers/image.helper";
+import { useUser } from "@/app/hooks/auth.hook";
 import { ITeam } from "@/app/interfaces/interfaces";
+import { useAppSelector } from "@/app/store/hooks";
+import { selectCurrentUser } from "@/app/store/slices/userSlice";
 import Image from "next/image";
 import React from "react";
 import { FaPlus, FaEdit, FaTrash } from "react-icons/fa";
@@ -9,24 +12,29 @@ interface TeamCardProps {
 }
 
 const TeamCard: React.FC<TeamCardProps> = ({ team }) => {
+  const { teamLeaderId } = team;
+  const user = useUser();
+  const isYourTeam = user ? teamLeaderId === user.id : false;
+
   return (
     <div className="bg-gray-100 border border-gray-300 shadow-md rounded-lg p-4 mb-4 flex flex-col gap-4">
       <div className="flex justify-between items-center">
         <h2 className="text-xl font-bold text-gray-900">{team.teamName}</h2>
-        <div className="flex gap-2">
-          <button className="text-green-500 hover:text-green-700">
-            <FaPlus size={16} />
-          </button>
-          <button className="text-blue-500 hover:text-blue-700">
-            <FaEdit size={16} />
-          </button>
-          <button className="text-red-500 hover:text-red-700">
-            <FaTrash size={16} />
-          </button>
-        </div>
+        {isYourTeam && (
+          <div className="flex gap-2">
+            <button className="text-green-500 hover:text-green-700">
+              <FaPlus size={16} />
+            </button>
+            <button className="text-blue-500 hover:text-blue-700">
+              <FaEdit size={16} />
+            </button>
+            <button className="text-red-500 hover:text-red-700">
+              <FaTrash size={16} />
+            </button>
+          </div>
+        )}
       </div>
-
-      <div className="flex items-center gap-4" >
+      <div className="flex items-center gap-4">
         <Image
           src={getAvatarUrl(team.teamLeader.avatarUrl)}
           alt="avatar"

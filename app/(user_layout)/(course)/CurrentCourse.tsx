609a45d64b17a18a67b5019974bf2bf06328c7f0
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-09-17 17:41:57 +0300

    Change styles and fix large text bug in CurrentCourse

--- app/(user_layout)/(course)/CurrentCourse.tsx
+++ app/(user_layout)/(course)/CurrentCourse.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import { getFullUrl } from "@/app/helpers/image.helper";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+
 interface CourseFormProps {
   courseId?: string;
 }
@@ -34,39 +35,37 @@ export const CurrentCourse = ({ courseId }: CourseFormProps) => {
   };
 
   const handleLessonClick = (lessonId: string) => {
-     router.push(`/current-lesson/${lessonId}`);
-
+    router.push(`/current-lesson/${lessonId}`);
   };
 
   return (
     <motion.div
       className="max-w-[90%] mx-auto p-12"
-      initial={{ opacity: 0, y: 20 }} 
-      animate={{ opacity: 1, y: 0 }} 
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
       <div className="flex flex-col gap-12 bg-white p-8 rounded-lg shadow-lg">
-        <div className="flex flex-col lg:flex-row gap-12">
-          <div className="w-full lg:w-1/2">
+        <div className="flex flex-col lg:flex-row items-start gap-6">
+          <div className="relative w-full lg:w-[300px] lg:h-[300px]">
             <Image
               src={getFullUrl(currentCourse.courseImageUrl)}
               alt={currentCourse.name}
-              className="w-full h-auto rounded-lg"
-              width={500}
-              height={500}
+              className="w-full h-full object-cover rounded-lg"
+              layout="fill"
               priority
             />
           </div>
 
           <div className="flex-1">
-            <h1 className="text-5xl font-bold text-gray-900 mb-6">
+            <h1 className="text-4xl font-bold text-gray-900 mb-4">
               {currentCourse.name}
             </h1>
-            <p className="text-xl text-gray-700 mb-8">
+            <p className="text-lg text-gray-700 mb-6">
               {currentCourse.description}
             </p>
 
-            <div className="flex items-center gap-6 mb-12">
+            <div className="flex items-center gap-4 mb-6">
               <Image
                 src={getFullUrl(currentCourse.author.avatarUrl)}
                 alt={`${currentCourse.author.firstName} ${currentCourse.author.lastName}`}
@@ -77,7 +76,7 @@ export const CurrentCourse = ({ courseId }: CourseFormProps) => {
               />
               <div>
                 <p className="text-lg text-gray-500">Author:</p>
-                <p className="text-2xl font-semibold text-gray-900">
+                <p className="text-xl font-semibold text-gray-900">
                   {currentCourse.author.firstName} {currentCourse.author.lastName}
                 </p>
               </div>
@@ -86,24 +85,26 @@ export const CurrentCourse = ({ courseId }: CourseFormProps) => {
         </div>
 
         <div className="w-full">
-          <h2 className="text-4xl font-semibold text-gray-800 mb-6">Lessons</h2>
+          <h2 className="text-3xl font-semibold text-gray-800 mb-6">Lessons</h2>
           {currentCourse.lessons && currentCourse.lessons.length > 0 ? (
             <ul>
               {currentCourse.lessons.map((lesson, index) => (
                 <motion.li
                   key={lesson.id}
                   onClick={() => handleLessonClick(lesson.id + '')}
-                  className={`p-4 rounded-lg mb-4 text-white cursor-pointer transition-transform transform ${
+                  className={`p-4 rounded-lg mb-4 cursor-pointer transition-transform transform ${
                     index % 2 === 0 ? "bg-gray-800" : "bg-gray-600"
                   }`}
                   initial={{ scale: 1 }}
-                  whileHover={{ scale: 1.05 }} 
+                  whileHover={{ scale: 1.05 }}
                   transition={{ duration: 0.2 }}
                 >
-                  <div className="text-2xl font-bold text-gray-200">
+                  <div className="text-2xl font-bold text-gray-200 mb-2">
                     {formatLessonNumber(index)}. {lesson.title}
                   </div>
-                  <p className="text-lg text-gray-300 mt-2">{lesson.text}</p>
+                  <p className="text-lg text-gray-300 mt-2 truncate">
+                    {lesson.text}
+                  </p>
                 </motion.li>
               ))}
             </ul>
